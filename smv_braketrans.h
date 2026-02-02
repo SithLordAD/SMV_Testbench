8ecbb90d73658c15d1a0405ac679bb3f5a5cf4b2
nevaRiaD <jaycee.alipio@gmail.com>
2026-02-02 14:07:39 -0800

    Removed smv enum library

--- smv_braketrans.h
+++ smv_braketrans.h
@@ -2,7 +2,6 @@
 #define _SMV_BRAKETRANS_H
 
 #include <stdint.h>
-#include "smv_board_enums.h"
 
 /* ---- Calibration Constants ---- */
 #define BRAKE_TRANS_SLOPE       125.0
@@ -11,9 +10,6 @@
 /* ---- Physical Limits ---- */
 #define PSI_MAX					500.0
 #define PSI_MIN					0.0
-
-/* ---- HSMessage for SMV Can Communication ---- */
-#define HSMESSAGE_PRESSURE      Pressure    /* HSMessage.Pressure from "smv_board_enums.h" */
 
 double ADCtoPSI(double voltage)
 {
