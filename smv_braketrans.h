a0c05b99265cd9cf64308669c1b132bfc076d776
nevaRiaD <jaycee.alipio@gmail.com>
2026-02-02 14:05:07 -0800

    Removed adc-voltage conversion since external adc already converts to voltage

--- smv_braketrans.h
+++ smv_braketrans.h
@@ -4,13 +4,6 @@
 #include <stdint.h>
 #include "smv_board_enums.h"
 
-/* ---- Hardware Constants ---- */
-#define BRAKE_TRANS_ADC_MAX     4095U
-#define BRAKE_TRANS_VREF        3.3         /* STM32 ADC reference voltage */
-#define BRAKE_TRANS_DIVIDER     2.0         /* Voltage divider ratio (sensor -> ADC) */
-#define BRAKE_TRANS_VMAX        4.5         /* Sensor full-scale voltage */
-#define BRAKE_TRANS_VMIN        0.5         /* Sensor zero-pressure voltage */
-
 /* ---- Calibration Constants ---- */
 #define BRAKE_TRANS_SLOPE       125.0
 #define BRAKE_TRANS_OFFSET      62.5
@@ -22,10 +15,9 @@
 /* ---- HSMessage for SMV Can Communication ---- */
 #define HSMESSAGE_PRESSURE      Pressure    /* HSMessage.Pressure from "smv_board_enums.h" */
 
-double ADCtoPSI(double adc)
+double ADCtoPSI(double voltage)
 {
-	double v_sensor = (adc * BRAKE_TRANS_VREF * BRAKE_TRANS_DIVIDER) / BRAKE_TRANS_ADC_MAX;
-	double psi = (BRAKE_TRANS_SLOPE * v_sensor) - BRAKE_TRANS_OFFSET;
+	double psi = (BRAKE_TRANS_SLOPE * voltage) - BRAKE_TRANS_OFFSET;
 	if (psi < PSI_MIN) psi = PSI_MIN;
 	if (psi > PSI_MAX) psi = PSI_MAX;
 
